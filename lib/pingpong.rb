47f456e70c08705e414b7a82efdcfd822bcd41f7
Joshua Lovelace <rakmial@gmail.com>
2020-08-25 15:05:06 -0700

    test 3 green

--- lib/pingpong.rb
+++ lib/pingpong.rb
@@ -4,8 +4,13 @@ class Integer
   def pingpong
     rv = (1..self).to_a
     rv.each do |el|
-      #binding.pry
-      if el.%(3) == 0
+      if el.%(5) == 0
+        if el.%(3) == 0
+          rv[el-1] = "ping-pong"
+        else
+          rv[el-1] = "pong"
+        end
+      elsif el.%(3) == 0
         rv[el-1] = "ping"
       end
     end
