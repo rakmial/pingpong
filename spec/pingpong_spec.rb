3a339d367a8f1b44e3b687b87d87f3337270b90c
Joshua Lovelace <rakmial@gmail.com>
2020-08-25 15:01:10 -0700

    test 3 red

--- spec/pingpong_spec.rb
+++ spec/pingpong_spec.rb
@@ -8,13 +8,16 @@ describe('pingpong') do
   it('search elements where modulo 3 == 0, replace with "ping"') do
     expect(3.pingpong).to(eq([1,2,'ping']))
   end
+  it('search elements where modulo 5 == 0, replace with "pong"') do
+    expect(5.pingpong).to(eq([1,2,"ping",4,"pong"]))
+  end
   
   #Final test
   #expect(7.pingpong).to(eq([1,2,'ping',4,'pong','ping',7]))
 end
 
 ### 'creates a list with number range 1..num'
+### 'each el mod 3 replace with "ping"'
 
-# 'each el mod 3 || 5 replace with "test"'
-# 'add control flow if el mod 3 replace with ping'
-# 'add control flow elif el mod 5 && not mod 3 replace with pong else replace ping-pong'
+# 'add control flow elif el mod 5 replace with pong'
+# 'add control flow under mod 3 if mod 5 replace ping-pong'
