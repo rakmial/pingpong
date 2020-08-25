f6c5f99f3fb411b5fbfc73a8573aed80f75fb55f
Joshua Lovelace <rakmial@gmail.com>
2020-08-25 14:52:03 -0700

    test 2 green

--- lib/pingpong.rb
+++ lib/pingpong.rb
@@ -3,10 +3,12 @@ require('pry')
 class Integer
   def pingpong
     rv = (1..self).to_a
-    #rv.each do |el|
-    #  if el.%(3||5) == 0
-    #    el = "test"
-    #  end
-    #end
+    rv.each do |el|
+      #binding.pry
+      if el.%(3) == 0
+        rv[el-1] = "ping"
+      end
+    end
+    rv
   end
 end
